40ce792623d9ada6cf20b0f5e4bc7d130b1f52da
Coding-Noob-Jimmy <162104413+Coding-Noob-Jimmy@users.noreply.github.com>
2024-05-07 16:35:04 +0800

    Revert: Delete IISRapi directory

--- IISRapi/model/data.py
+++
@@ -1,8 +0,0 @@
-from typing import NamedTuple,List,Tuple
-
-class Data(NamedTuple):
-    ori_txt: str
-    ret_txt: str=None
-    ner_tags: List[Tuple[str, int, int]] = None
-    punct: List[Tuple[str, int]] = None
-
